e4b6949a69b42480517e72bd5b7460a78c4315f4
unknown <beata.krasnopolska@interia.pl>
2018-07-04 20:17:06 +0200

    Add contact subpage

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -30,3 +30,12 @@ $('.slick-slider').slick({
       }
     ]
   });
+// MAP IN CONTACT SUBPAGE
+  function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(50.05, 19.94),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    }
